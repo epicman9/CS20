588fd36b5e39c63082539e724198abbc5c741d6c
34362001 <34362001@L815WD150037.edu.cbe.ab.ca>
2022-03-03 11:44:56 -0700

    Began work on chapter 5

---
+++ Chapter5/src/Evens.java
@@ -0,0 +1,22 @@
+/*
+
+Program: Evens.java          Last Date of this Revision: March 3rd, 2022
+
+Purpose: Create an evens application that displays the even numbers between 1 and 20, inclusive.
+
+Author: Ewan Brady
+School: CHHS
+Course: Computer Programming 20
+  
+
+*/
+public class Evens 
+{
+
+	public static void main(String[] args) 
+	{
+
+
+	}
+
+}
