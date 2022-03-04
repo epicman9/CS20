a069e0584df04e6536308f5d4c7fbb68d242cd53
34362001 <34362001@L815WD150037.edu.cbe.ab.ca>
2022-03-04 10:01:16 -0700

    even more chapter 5 work

---
+++ Chapter5/src/OddSum.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+/*
+
+Program: OddSum.java          Last Date of this Revision: March 4th, 2022
+
+Purpose: Create a OddSum application that prompts the user for a number then sums the odd numbers from 1 to the number entered. 
+
+Author: Ewan Brady
+School: CHHS
+Course: Computer Programming 20
+  
+
+*/
+public class OddSum 
+{	
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int num = 0;
+		
+		System.out.println("Enter an integer: ");
+		num = input.nextInt();
+		
+		int bignum = 0;
+		
+		for(int i = 1; i <= num;)
+		{
+			System.out.println(i);
+			
+			bignum = bignum + i;
+			i = i + 2;
+		}
+		
+		System.out.println("The sum is: " + bignum);
+		
+		input.close();
+	}
+}
+/* Screen Dump
+
+Enter an integer: 
+9
+1
+3
+5
+7
+9
+The sum is: 25
+
+Enter an integer: 
+16
+1
+3
+5
+7
+9
+11
+13
+15
+The sum is: 64
+
+Enter an integer: 
+6
+1
+3
+5
+The sum is: 9
+
+Enter an integer: 
+20
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+The sum is: 100
+
+
+*/
